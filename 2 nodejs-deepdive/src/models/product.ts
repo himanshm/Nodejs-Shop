5680d888de52839c1613a8e42fedfb0fb3f23211
Himanshu Mishra <sai.himanshum011@gmail.com>
2024-03-13 22:11:42 +0530

    Add a product id to the path

--- 2 nodejs-deepdive/src/models/product.ts
+++ 2 nodejs-deepdive/src/models/product.ts
@@ -1,8 +1,10 @@
 import fs from 'fs';
 import path from 'path';
 import directoryName from '../util/path';
+import generateUniqueId from '../util/generateId';
 
 interface ProductType {
+  id: string;
   title: string;
   imageUrl: string;
   description: string;
@@ -11,6 +13,8 @@ interface ProductType {
 
 // const products: ProductType[] = [];
 const newPath = path.join(directoryName, 'data', 'products.json');
+// https://images.pexels.com/photos/10252340/pexels-photo-10252340.jpeg?auto=compress&cs=tinysrgb&w=800
+// It follows Harry Potter, a wizard in his fourth year at Hogwarts School of Witchcraft and Wizardry, and the mystery surrounding the entry of Harry's name into the Triwizard Tournament, in which he is forced to compete. The book was published in the United Kingdom by Bloomsbury and in the United States by Scholastic.
 
 const getProductsFromFile = (cb: (products: ProductType[]) => void) => {
   fs.readFile(newPath, 'utf-8', (err, fileContent) => {
@@ -23,16 +27,20 @@ const getProductsFromFile = (cb: (products: ProductType[]) => void) => {
 };
 
 export class Product {
+  id: string;
   static products: ProductType[] = [];
   constructor(
     public title: string,
     public imageUrl: string,
     public description: string,
     public price: number
-  ) {}
+  ) {
+    this.id = generateUniqueId();
+  }
 
   save() {
     getProductsFromFile((products) => {
+      console.log(this);
       products.push(this);
       fs.writeFile(newPath, JSON.stringify(products), (err) => {
         console.log(err);
