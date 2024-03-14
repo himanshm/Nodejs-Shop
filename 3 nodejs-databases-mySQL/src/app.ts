c8ad2b6d939c87c01471313bf7a1d877252356b5
Himanshu Mishra <sai.himanshum011@gmail.com>
2024-03-14 21:22:32 +0530

    Final Commit

--- 3 nodejs-databases-mySQL/src/app.ts
+++ 3 nodejs-databases-mySQL/src/app.ts
@@ -1,9 +1,10 @@
 import express, { Express } from 'express';
 import path from 'path';
 import bodyParser from 'body-parser';
-import adminRoutes from './routes/admin';
-import shopRoutes from './routes/shop';
+// import adminRoutes from './routes/admin';
+// import shopRoutes from './routes/shop';
 import { get404Page } from './controllers/error';
+import sequelize from './util/database';
 
 const app: Express = express();
 
@@ -14,9 +15,15 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+// app.use('/admin', adminRoutes);
+// app.use(shopRoutes);
 
 app.use(get404Page);
 
-app.listen(3000);
+sequelize
+  .sync()
+  .then((result) => {
+    // console.log(result);
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err));
