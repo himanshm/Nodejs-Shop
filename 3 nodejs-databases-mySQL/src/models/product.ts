9522ab412c99562eba8b9fd7fba2e63062fbe39e
Himanshu Mishra <sai.himanshum011@gmail.com>
2024-03-14 21:03:18 +0530

    Define a model - Product Model

--- 3 nodejs-databases-mySQL/src/models/product.ts
+++ 3 nodejs-databases-mySQL/src/models/product.ts
@@ -1,38 +1,26 @@
-import { Cart } from './cart';
-import db from '../util/database';
-import { RowDataPacket } from 'mysql2';
+import { Sequelize, DataTypes } from 'sequelize';
+import sequelize from '../util/database';
 
-export class Product {
-  static products = [];
-  constructor(
-    public id: string | null, // Simply pass null for a new product
-    public title: string,
-    public imageUrl: string,
-    public description: string,
-    public price: number
-  ) {}
+const Product = sequelize.define('Product', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  title: DataTypes.STRING,
+  price: {
+    type: DataTypes.DOUBLE,
+    allowNull: false,
+  },
+  imageUrl: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
 
-  save() {
-    return db.execute(
-      'INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)',
-      [this.title, this.price, this.imageUrl, this.description]
-    );
-  }
-
-  static deleteById(id: string) {}
-
-  static async fetchAll(): Promise<RowDataPacket[]> {
-    const [rows, _] = await db.execute<RowDataPacket[]>(
-      'SELECT * FROM products'
-    );
-    return rows;
-  }
-
-  static async findById(id: number): Promise<RowDataPacket> {
-    const [rows, _] = await db.execute<RowDataPacket[]>(
-      'SELECT * FROM products WHERE products.id = ?',
-      [id]
-    );
-    return rows[0];
-  }
-}
+export default Product;
